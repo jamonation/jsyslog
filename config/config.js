670da052f8c788c75e71d3cdd0908531b3dc3356
Jamon Camisso <jamonation@gmail.com>
2015-07-19 17:04:39 -0400

    refactored modules and logger into better module.exports format

--- config/config.js
+++ config/config.js
@@ -2,26 +2,26 @@
 var config = {}
 
 // where to write a pid file - convention is /var/run/
-config.pid = '/var/run/jsyslog.pid'
+config.pid = '/tmp/jsyslog.pid'
 
 // use UDP 514 for actual syslog port per RFC 3164
 // TODO: use TCP 6514 for TLS syslog port per RFC 5425
-config.port = 514
+config.port = 5514
 
 // bind to which network interface? ipv4 only at the moment
 // TODO: make a flag to specify v4/v6
 config.iface = '0.0.0.0'
 
 // what is the socket's path e.g. /dev/log on most linux systems
-config.socketPath = '/dev/log'
+config.socketPath = '/tmp/log'
 
 // do we rm and take over an existing /dev/log socket?
 // very dangerous on a prod system with another syslog daemon installed
-config.claimSocket = true
+config.claimSocket = false
 
 // where to log, on debian/ubuntu use /var/log/syslog
 // on rhel systems use /var/log/messages
-config.syslogFile = '/var/log/syslog'
+config.syslogFile = '/tmp/syslog'
 
 // date format, depending on which rfc is being used, change the strftime format here
 // e.g. rfc 3164 uses %b %e %H:%M:%S where %b is short month, and %e is date with a leading space
