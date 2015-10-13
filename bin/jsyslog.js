cd545e089f61e1d0f9e7d00034f689678b57e9aa
Jamon Camisso <jamonation@gmail.com>
2015-10-13 16:04:57 -0400

    WIP: test(tape) adding tests for bin/jsyslog to check various signals and conditions

--- bin/jsyslog.js
+++ bin/jsyslog.js
@@ -4,6 +4,8 @@ var bluebird = require('bluebird')
 var fs = bluebird.promisifyAll(require('fs'))
 var spawn = require('child_process').spawn
 
+var processPromise = bluebird.promisifyAll(process)
+
 // only want start/stop/restart/status argument
 var arg = process.argv.slice(2)[0]
 
@@ -11,7 +13,7 @@ var config = require('../config/config')
 
 
 // start the child jsyslog process and detach parent
-function spawn_child() {
+var spawn_child = function() {
   var child = spawn('node', ['./lib/jsyslog'], {
     detached: true,
     stdio: [ 'ignore', 'ignore', 'ignore' ]
@@ -23,106 +25,163 @@ function spawn_child() {
     })  
     .catch(function(err) {
       console.log('Could not write process pid, attempting to shutdown jsyslog')
-      shutdown(child.pid).then(process.exit(1)) // something went splat, check file permissions?
+      child.kill('SIGTERM').then(process.exit(1)) // something went splat, check file permissions?
     })
 }
 
 
+/*
 // shutdown a running jsyslog child if a pid exists
 function signal_pid(pid, signal) {
-  var d = bluebird.defer()
-  process.kill(pid, signal, function(err) {
-    if (err) {
-      console.log('Error sending %s to process %d', signal, pid)
-      d.reject(err)
+    var d = bluebird.defer()
+    process.kill(pid, signal, function(err) {
+	if (err) {
+	    console.log('Error sending %s to process %d', signal, pid)
+	    d.reject(err)
+	}
+    })
+    if (signal === 'SIGTERM') {
+	console.log('Sent signal %s to process %d', signal, pid)
     }
-  })
-  if (signal === 'SIGTERM') {
-    console.log('Sent signal %s to process %d', signal, pid)
-  }
-  d.resolve()
-  return d.promise
+    d.resolve()
+    return d.promise
+}*/
+
+
+// shutdown a running jsyslog child if a pid exists
+var signal_pid = function(pid, signal) {
+    // this is stupid and shouldn't be required. write better tests.
+    // right now, if a socket exists, e.g. after testing and jsyslog stop is called
+    // it will throw an ESRCH exception. this should not happen. stupid.
+    if (process.env.MODE !== 'TESTING') {
+	return fs.unlinkAsync(config.pid)
+	    .then(function() {
+		fs.unlinkAsync(config.socketPath)
+	    })
+	    .then(function() {
+		processPromise.killAsync(pid, signal)
+	    })
+	    .then(function() {
+		console.log('Server stopped. Exiting.')
+            })
+	    .catch(Error, error_pid)	
+            .catch(function(err) {
+		return err
+	    })
+    } else {
+      	return processPromise.killAsync(pid, signal)
+	    .then(function() {
+		fs.unlink(config.socketPath)   
+	    })	
+	    .catch(Error, error_pid)
+    }
 }
 
 
-function log_pid(pid) {
+var log_pid = function(pid) {
   console.log('jsyslog pidfile %s with pid %d exists', config.pid, pid)
   return pid  
 }
 
 
-function error_pid(err) {
+var error_pid = function(err) {
   if (err.errno === 'ESRCH') {
     // called stop on a pid for a process that doesn't exist. Bork.
-    console.log('No jsyslog process responding to signals at pid %s', fs.readFileSync(config.pid))
-    //console.log('Please check or remove %s.', config.pid)
-    //console.log('Also check for a rogue (still running) jsyslog.js process')
-    throw new Error(err)
-    process.exit(1)  
+    console.log('No jsyslog process responding to signals at pid %s. Exiting.', fs.readFileSync(config.pid))
+    /* istanbul ignore if */        
+    if (process.env.MODE !== 'TESTING') {
+      //throw new Error(err)	  
+      process.exit(1)
+    } else {
+      return err
+    }
   } else if (err.code === 'ENOENT') {
     console.log('No jsyslog process detected.')
-    process.exit()
+    /* istanbul ignore if */
+    if (process.env.MODE !== 'TESTING') {  
+      process.exit()
+    } else {
+      return err
+    }
   }
 }
 
+var daemon_action = function(action) {
+    // parse arg string for start/restart/stop and take appropriate action
+    if (arg === 'start') {
+	fs.readFileAsync(config.pid) // check for a pid
+	    // pid exists, so just notify the user and then take no action
+	    .then(log_pid)
+	    // pid doesn't exist, so start up a child
+	    .catch(Error, function(err) {
+		if (err.code === 'ENOENT') {
+		    spawn_child()
+		}
+	    })
+	    // bustage
+	    .catch(function(err) {
+		console.log('Something went wrong starting jsyslog, exiting')
+		throw new Error(err)
+		process.exit(1)
+	    })
+    } else if (arg === 'restart') {
+	fs.readFileAsync(config.pid) // check for a pid
+	    .then(function(pid) {
+		signal_pid(pid, 'SIGTERM') // be lazy, shutdown the server
+	    })
+	    .then(spawn_child) // then start up a new one
+	    .catch(spawn_child) // a process doesn't exist, so start one before really failing harrrrrd
+	    .catch(function(err) {
+		console.log('Something went wrong restarting. Exiting.')
+		throw new Error(err)  
+		process.exit(1)
+	    })
+    } else if (arg === 'stop') {
+	fs.readFileAsync(config.pid) // check for a pid
+	    .then(function(pid) {
+		signal_pid(pid, 'SIGTERM') // shutdown the server
+		    .catch(Error, error_pid) // gracefully handle ENOENT/ESRCH errors
+	    })
+	    .catch(Error, error_pid) // gracefully handle ENOENT/ESRCH errors
+	    .catch(function(err) {
+	        console.dir(err)	    
+		console.log('Something went wrong shutting down.')
+		throw new Error(err)  
+		process.exit(1)
+	    })
+    } else if (arg === 'status') {
+	fs.readFileAsync(config.pid) // check for a pid
+	    .then(log_pid)
+	    .then(function(pid) {
+		signal_pid(pid, 'SIGCHLD') // check the server exists
+		  .catch(Error, error_pid) // gracefully handle ENOENT/ESRCH errors
+            })
+	    .catch(Error, error_pid) // gracefully handle ENOENT/ESRCH errors
+            .catch(function(err) {
+		console.log('\n\nSomething went disastrously wrong:')
+		console.error('It looks like there is a detatched process with the wrong pid.')
+		console.error('You MUST do the following:\n')
+		console.error('1) Send SIGKILL signal to the running node ./lib/jsyslog.js process.')
+		console.error('2) Remove the pid file in %s', config.pid)
+		throw new Error(err)  
+		process.exit(1)
+	    })
+    } else {
+	console.error('Error. No argument given.\n' +
+		      'Please pass \'start\', \'stop\' or \'restart\' as an argument to jsyslog')
+    }
 
-// parse arg string for start/restart/stop and take appropriate action
-if (arg === 'start') {
-  fs.readFileAsync(config.pid) // check for a pid
-    // pid exists, so just notify the user and then take no action
-    .then(log_pid)
-    // pid doesn't exist, so start up a child
-    .catch(Error, function(err) {
-      if (err.code === 'ENOENT') {
-        spawn_child()
-      }
-    })
-    // bustage
-    .catch(function(err) {
-       console.log('Something went wrong starting jsyslog, exiting')
-       throw new Error(err)
-       process.exit(1)
-    })
-} else if (arg === 'restart') {
-  fs.readFileAsync(config.pid) // check for a pid
-    .then(function(pid) {
-      signal_pid(pid, 'SIGTERM') // be lazy, shutdown the server
-    })
-    .then(spawn_child) // then start up a new one
-    .catch(function(err) {
-      console.log('Something went wrong restarting')
-      throw new Error(err)  
-      process.exit(1)
-    })
-} else if (arg === 'stop') {
-  fs.readFileAsync(config.pid) // check for a pid
-    .then(function(pid) {
-      signal_pid(pid, 'SIGTERM') // shutdown the server
-    })
-    .catch(Error, error_pid)
-    .catch(function(err) {
-      console.log('Something went wrong shutting down.')
-      throw new Error(err)  
-      process.exit(1)
-    })
-} else if (arg === 'status') {
-  fs.readFileAsync(config.pid) // check for a pid
-    .then(log_pid)
-    .then(function(pid) {
-      signal_pid(pid, 'SIGCHLD') // check the server exists
-    })
-    .catch(Error, error_pid)
-    .catch(function(err) {
-      console.log('\n\nSomething went disastrously wrong:')
-      console.error('It looks like there is a detatched process with the wrong pid.')
-      console.error('You MUST do the following:\n')
-      console.error('1) Send SIGKILL signal to the running node ./lib/jsyslog.js process.')
-      console.error('2) Remove the pid file in %s', config.pid)
-      throw new Error(err)  
-      process.exit(1)
-    })
-} else {
-  console.error('Error. No argument given.\n' +
-                'Please pass \'start\', \'stop\' or \'restart\' as an argument to jsyslog')
 }
 
+if (process.env.MODE !== 'TESTING') {
+    daemon_action(arg)
+}
+
+module.exports = {
+    arg: arg,
+    daemon_action: daemon_action,
+    error_pid: error_pid,
+    log_pid: log_pid,
+    signal_pid: signal_pid,
+    spawn_child: spawn_child
+}
--- tests/jsyslog.js
+++ tests/jsyslog.js
@@ -1,12 +1,58 @@
 var test = require('tape')
 
+var dgram = require('dgram')
+var fs = require('fs')
+var net = require('net')
 
-test('jsyslog',
+test('jsyslog lib',
      function (t) {
 	 t.plan(1)
 
 	 var jsyslog = require('../lib/jsyslog')
 	 
 	 t.equal(typeof jsyslog, 'object', 'jsyslog is imported')
+	 var server = {}
+	 server.sock = net.createServer()
+	 server.sock.listen('/tmp/jsyslog.test.sock')
+	 server.dgram = dgram.createSocket('udp4')
+	 server.dgram.bind(55514, 'localhost')
+	 jsyslog.shutdown(server)
+     }
+)
+
+test('jsyslog bin',
+     function (t) {
+	 //t.plan(6)
+
+	 var jsyslog = require('../bin/jsyslog')
+	 var config = require('../config/config.js')	 
+	 
+	 t.equal(typeof jsyslog, 'object', 'jsyslog is imported')
+         jsyslog.daemon_action('status')
+	 jsyslog.spawn_child()
+	 t.equal(jsyslog.log_pid(process.pid), process.pid, 'log_pid returns correct process.pid')
+	 var sigusr = jsyslog.signal_pid(process.pid, 'SIGUSR1')
+	 t.equal(typeof sigusr, 'object', 'SIGUSR1 sent to signal_pid returns a promise')
+
+	 // this test is a little funky, needs to catch the promise rejection, which is a thrown error
+	 jsyslog.signal_pid(process.pid, 'SIGFOO')
+	     .catch(function(err) { t.throws(err, 'signal SIGFOO throws an error') })
+
+         // try to kill a non-existent pid and catch the rejected promise
+	 jsyslog.signal_pid(100000000, 'SIGTERM')
+	     .catch(function(err) { t.throws(err, 'sigterm sent to non-existent pid throws an error') }) 
+				    
+	 var err_enoent = new Error('ENOENT, readlink /tmp/foo')
+         err_enoent.code = 'ENOENT'
+         err_enoent.errno = 34
+         err_enoent.path = '/tmp/foo'
+	 t.throws(jsyslog.error_pid(err_enoent), 'error_pid sent ENOENT returns error in place of process.exit')
+
+	 var err_esrch = new Error('ESRCH, readlink /tmp/foo')
+         err_esrch.code = 'ESRCH'
+         err_esrch.errno = 'ESRCH'
+	 t.throws(jsyslog.error_pid(err_esrch), 'error_pid sent ESRCH returns error in place of process.exit')
+
+         t.end()
      }
 )
